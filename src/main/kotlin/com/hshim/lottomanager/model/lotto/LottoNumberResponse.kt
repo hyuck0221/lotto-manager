a99f92dcea611aad167205ae7776c915216e0698
심혁 <shimhyuck@simhyeog-ui-MacBookPro.local>
2024-12-31 13:45:13 +0900

    화면 추가

--- src/main/kotlin/com/hshim/lottomanager/model/lotto/LottoNumberResponse.kt
+++ src/main/kotlin/com/hshim/lottomanager/model/lotto/LottoNumberResponse.kt
@@ -1,24 +1,15 @@
 package com.hshim.lottomanager.model.lotto
 
-import com.hshim.lottomanager.database.account.User
-import com.hshim.lottomanager.database.lotto.Lotto
 import com.hshim.lottomanager.database.lotto.LottoNumber
 
 class LottoNumberResponse(
     val numbers: List<Int>,
+    val rank: Int?,
     val lotto: LottoResponse,
 ) {
-    fun toEntity(
-        lotto: Lotto,
-        user: User,
-    ) = LottoNumber(
-        lotto = lotto,
-        user = user,
-        numbers = numbers,
-    )
-
     constructor(lottoNumber: LottoNumber): this (
         numbers = lottoNumber.numbers,
+        rank = lottoNumber.rank,
         lotto = LottoResponse(lottoNumber.lotto),
     )
 }
